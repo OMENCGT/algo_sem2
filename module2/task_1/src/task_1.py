927f0e556b2f26809b7f26dacc64d0c7ff368b96
OMENCGT <55311553+OMENCGT@users.noreply.github.com>
2025-09-10 23:38:02 +0300

    Create task_1.py
    add mod2 task1

---
+++ module2/task_1/src/task_1.py
@@ -0,0 +1,41 @@
+from typing import List, Tuple
+import sys, time
+
+def in_order(g: List[Tuple[int, int, int]], v:int):
+    if g[v][1] > -1:
+        in_order(g, g[v][1])
+    print(g[v][0], end=' ')
+    if g[v][2] > -1:
+        in_order(g, g[v][2])
+
+def pre_order(g: List[Tuple[int, int, int]], v:int):
+    print(g[v][0], end=' ')
+    if g[v][1] > -1:
+        pre_order(g, g[v][1])
+    if g[v][2] > -1:
+        pre_order(g, g[v][2])
+
+def post_order(g: List[Tuple[int, int, int]], v:int):
+    if g[v][1] > -1:
+        post_order(g, g[v][1])
+    if g[v][2] > -1:
+        post_order(g, g[v][2])
+    print(g[v][0], end=' ')
+
+def task1():
+    n = int(input())
+    a = [tuple(map(int, input().split())) for i in range(n)]
+    in_order(a, 0)
+    print()
+    pre_order(a, 0)
+    print()
+    post_order(a, 0)
+
+
+if __name__ == '__main__':
+    f = open("input.txt", "r")
+    sys.stdin = f
+    t_s = time.time_ns()
+    task1()
+    t_e = time.time_ns()
+    print(f"total time: {(t_e - t_s) / (10 ** 9)} seconds")
